c88b0ca9ff6d8e4de3462449d44c6d8487bf441e
ivanzheng123 <ivanzheng456@gmail.com>
2025-03-18 12:32:11 -0400

    Update todos.ts

--- src/data/todos.ts
+++ src/data/todos.ts
@@ -2,18 +2,5 @@ import { Todo } from "../types/todo";
 
 export const dummyData: Todo[] = [
   {
-    id: 1,
-    title: "I have gigantic pp",
-    completed: false,
-  },
-  {
-  id: 2,
-  title: "I need to do my baby",
-  completed: false,
-  },
-  {
-    id: 3,
-    title: "I need to kiss baby",
-    completed: false,
   },
 ];
